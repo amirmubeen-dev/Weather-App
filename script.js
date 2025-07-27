b1c8d3f3523ff54969612951dfdf05024a842678
Amir Mubeen <amirmubeen.mw@gmail.com>
2025-07-27 12:00:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const apiKey = "b20ee4c81ea9d725e0a3b0be24839349";
+
+async function getWeather() {
+  const city = document.getElementById("cityInput").value;
+  const resultDiv = document.getElementById("weatherResult");
+
+  if (!city) {
+    resultDiv.innerHTML = "<p>Please enter a city name.</p>";
+    return;
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error("City not found");
+    const data = await response.json();
+
+    resultDiv.innerHTML = `
+      <h2>${data.name}, ${data.sys.country}</h2>
+      <p>Temperature:${data.main.temp}°C</p>
+      <p>Weather:${data.weather[0].description}</p>
+      <p>Humidity: ${data.main.humidity}%</p>
+      <p>Wind Speed: ${data.wind.speed} m/s</p>
+    `;
+  } catch (error) {
+    resultDiv.innerHTML = `<p>Error: ${error.message}</p>`;
+  }
+}
